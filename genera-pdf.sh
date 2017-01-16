0b862dc13ae0f0a3ac0ab380b859e842af312229
jmunoz298 <juanmunoz.juan@gmail.com>
2017-01-17 00:49:01 +0100

    Nuevo commit

--- genera-pdf.sh
+++ genera-pdf.sh
@@ -1,1 +1,9 @@
-bundle exec asciidoctor-pdf -a pdf-style=custom-theme.yml -r asciidoctor-bibtex 00-atlasti.adoc  -o atlasti.pdf
+bundle exec asciidoctor-pdf -b pdf -a pdf-style=custom-theme.yml -r asciidoctor-diagram -r asciidoctor-bibtex 00-atlasti.adoc  -o atlasti.pdf
+
+## asciidoctor-pdf: generador archivos pdf
+## -b pdf: define el formato de salida (e influye en condiciones if, para que determinados contenidos se muestren sólo para html/pdf)
+## -a pdf-style=custom-theme.yml: estilo del pdf ("similar" a css)
+## -r asciidoctor-diagram: opción para generar diagramas (mermaid, diita...)
+## -r asciidoctor-bibtex: opción generar bibliografía a partir de archivo bibtex
+## 00-atlasti.adoc: archivo de entrada
+## -o atlasti.html: archivo de salida
