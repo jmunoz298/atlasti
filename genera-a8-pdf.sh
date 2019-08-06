c82ae669d7a353e56e23ceb5750d672d363b66ef
jmunoz298 <juanmunoz.juan@gmail.com>
2019-08-06 04:39:03 +0200

    Actualizados archivos genera

--- genera-a8-pdf.sh
+++
@@ -1,9 +0,0 @@
-bundle exec asciidoctor-pdf -b pdf -a pdf-style=custom-theme.yml -r asciidoctor-diagram -r asciidoctor-bibtex atlasti8.adoc  -o atlasti8.pdf
-
-## asciidoctor-pdf: generador archivos pdf
-## -b pdf: define el formato de salida (e influye en condiciones if, para que determinados contenidos se muestren sólo para html/pdf)
-## -a pdf-style=custom-theme.yml: estilo del pdf ("similar" a css)
-## -r asciidoctor-diagram: opción para generar diagramas (mermaid, diita...)
-## -r asciidoctor-bibtex: opción generar bibliografía a partir de archivo bibtex
-## 00-atlasti.adoc: archivo de entrada
-## -o atlasti.html: archivo de salida
--- genera.sh
+++ genera.sh
@@ -1,9 +1,11 @@
-asciidoctor -r asciidoctor-bibtex -r asciidoctor-diagram -b html5  $1.adoc -o $1.html
+## ./genera.sh 
+asciidoctor -r asciidoctor-bibtex -r asciidoctor-diagram -b html5  atlasti8.adoc -o atlasti8.html
+
+## asciidoctor -r asciidoctor-bibtex -r asciidoctor-diagram -b html5  $1.adoc -o $1.html
 
 ## -r asciidoctor-diagram: opción para generar diagramas (mermaid, diita...)
 ## -r asciidoctor-bibtex: opción generar bibliografía a partir de archivo bibtex
 ## -b html5: define el formato de salida (e influye en condiciones if, para que determinados contenidos se muestren sólo para html/pdf)
-## 00-atlasti.adoc: archivo de entrada
-## -o atlasti.html: archivo de salida
+
 
 # bundle exec asciidoctor -r asciidoctor-bibtex 00-atlasti.adoc -o atlasti.html
